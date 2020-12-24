901f464a262c8ab5943e98548ae93ae5ec56e57b
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-12-23 22:18:11 -0400

    alterando horario

--- src/jobs/productsJob.ts
+++ src/jobs/productsJob.ts
@@ -116,7 +116,7 @@ const cronStart = async () => {
   }
 };
 
-const everyDayAtMidnight = '15 02 * * *';
+const everyDayAtMidnight = '20 02 * * *';
 
 const job = new CronJob(everyDayAtMidnight, cronStart, null, true);
 
